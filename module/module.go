1d680e64aa69ad5a6f6d4fd9ee3322c09cd4cbd5
aihailin <anhoder@88.com>
2020-10-11 23:19:04 +0800

    add. goroutine chan

--- module/module.go
+++ module/module.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	. "fmt"
+
+	_ "reflect" // 匿名导入，只会执行包的初始化函数
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -8,4 +12,9 @@ func main() {
 	log.WithFields(log.Fields{
 		"animal": "walrus",
 	}).Info("log test")
+	Println("fmt Println")
+}
+
+func init() {
+	Println("init...")
 }
